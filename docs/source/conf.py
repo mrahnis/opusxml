6e7306159c22246526a995a3e4c8c35679094cf3
Mike Rahnis <mike.rahnis@gmail.com>
2016-10-19 11:06:13 -0400

    add to list of mocks

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ class Mock(object):
 		else:
 			return Mock()
 
-MOCK_MODULES = ['lxml','fiona','shapely']
+MOCK_MODULES = ['lxml','fiona', 'fiona.crs', 'shapely', 'shapely.geometry']
 for mod_name in MOCK_MODULES:
 	sys.modules[mod_name] = Mock()
 
